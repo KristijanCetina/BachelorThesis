693be3d5e75b5601032027576faceacf00329600
Kristijan Cetina <kristijan.cetina@gmail.com>
2019-04-07 18:27:23 +0200

    Create LoggingWithTemp.ino

---
+++ arduinoSource/LoggingWithTemp/LoggingWithTemp.ino
@@ -0,0 +1,305 @@
+#include <SPI.h>
+#include <Arduino.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+#include <SD.h>
+#include <avr/sleep.h>
+#include <Wire.h>         // this #include still required because the RTClib depends on it
+#include "RTClib.h"
+
+int voltage;
+int Temperatura;
+int TempSenzor = A0;
+
+#define aref_voltage 3.3
+float temp[10] = { 0 };
+
+float i = 0;
+int n = 0;
+float median = 0;
+
+// Ladyada's logger modified by Bill Greiman to use the SdFat library
+//
+// This code shows how to listen to the GPS module in an interrupt
+// which allows the program to have more 'freedom' - just parse
+// when a new NMEA sentence is available! Then access data when
+// desired.
+//
+// Tested and works great with the Adafruit Ultimate GPS Shield
+// using MTK33x9 chipset
+//    ------> http://www.adafruit.com/products/
+// Pick one up today at the Adafruit electronics shop
+// and help support open source hardware & software! -ada
+// Fllybob added 10 sec logging option
+SoftwareSerial mySerial(8, 7);
+Adafruit_GPS GPS(&mySerial);
+
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  false
+/* set to true to only log to SD when GPS has a fix, for debugging, keep it false */
+#define LOG_FIXONLY true
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+#ifndef ESP8266 // Sadly not on ESP8266
+boolean usingInterrupt = false;
+#endif
+
+// Set the pins used
+#define chipSelect 10
+#define ledPin 13
+
+File logfile;
+
+RTC_DS1307 RTC;  // define the Real Time Clock object
+RTC_Millis rtc;
+
+ char timestamp[30];
+// call back for file timestamps
+void dateTime(uint16_t* date, uint16_t* time) {
+ DateTime now = RTC.now();
+ sprintf(timestamp, "%02d:%02d:%02d %2d/%2d/%2d \n", now.hour(),now.minute(),now.second(),now.month(),now.day(),now.year()-2000);
+ Serial.println("yy");
+ Serial.println(timestamp);
+ // return date using FAT_DATE macro to format fields
+ *date = FAT_DATE(now.year(), now.month(), now.day());
+
+ // return time using FAT_TIME macro to format fields
+ *time = FAT_TIME(now.hour(), now.minute(), now.second());
+}
+
+
+// read a Hex value and return the decimal equivalent
+uint8_t parseHex(char c) {
+  if (c < '0')
+    return 0;
+  if (c <= '9')
+    return c - '0';
+  if (c < 'A')
+    return 0;
+  if (c <= 'F')
+    return (c - 'A')+10;
+}
+
+// blink out an error code
+void error(uint8_t errno) {
+  /*
+  if (SD.errorCode()) {
+   putstring("SD error: ");
+   Serial.print(card.errorCode(), HEX);
+   Serial.print(',');
+   Serial.println(card.errorData(), HEX);
+   }
+   */
+  while(1) {
+    uint8_t i;
+    for (i=0; i<errno; i++) {
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+      delay(100);
+    }
+    for (i=errno; i<10; i++) {
+      delay(200);
+    }
+  }
+}
+
+void setup() {
+  Wire.begin();
+ if (!RTC.begin()) {
+   Serial.println("RTC failed");
+   while(1);
+ };
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("\r\nUltimate GPSlogger Shield");
+  pinMode(ledPin, OUTPUT);
+  pinMode(TempSenzor, INPUT);                        //postavi izvod TempSenzor (A0) kao ulazni
+  analogReference(EXTERNAL);
+  
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card init. failed!");
+    error(2);
+  }
+  char filename[15];
+  strcpy(filename, "GPSLOG00.csv");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = '0' + i/10;
+    filename[7] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if( ! logfile ) {
+    Serial.print("Couldnt create ");
+    Serial.println(filename);
+    error(3);
+  }
+  Serial.print("Writing to ");
+  Serial.println(filename);
+
+  // connect to the GPS at the desired rate
+  GPS.begin(9600);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For logging data, we don't suggest using anything but either RMC only or RMC+GGA
+  // to keep the log files at a reasonable size
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 100 millihertz (once every 10 seconds), 1Hz or 5Hz update rate
+
+  // Turn off updates on antenna status, if the firmware permits it
+  GPS.sendCommand(PGCMD_NOANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+#ifndef ESP8266 // Not on ESP8266
+  useInterrupt(true);
+#endif
+
+  Serial.println("Ready!");
+}
+
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+#ifndef ESP8266 // Not on ESP8266
+ISR(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  #ifdef UDR0
+      if (GPSECHO)
+        if (c) UDR0 = c;
+      // writing direct to UDR0 is much much faster than Serial.print
+      // but only one character can be written at a time.
+  #endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  }
+  else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+#endif // ESP8266
+
+// function to sort the array in ascending order
+void Array_sort(float *array, int n)
+{
+  // declare some local variables
+  int i = 0, j = 0, temp = 0;
+
+  for (i = 0; i<n; i++)
+  {
+    for (j = 0; j<n - 1; j++)
+    {
+      if (array[j]>array[j + 1])
+      {
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+
+float Find_median(float array[], int n)
+{
+  float median = 0;
+
+  // if number of elements are even
+  if (n % 2 == 0)
+    median = (array[(n - 1) / 2] + array[n / 2]) / 2.0;
+  // if number of elements are odd
+  else
+    median = array[n / 2];
+
+  return median;
+}
+
+void loop(){
+  DateTime now = rtc.now();
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+
+    // Don't call lastNMEA more than once between parse calls!  Calling lastNMEA
+    // will clear the received flag and can cause very subtle race conditions if
+    // new data comes in before parse is called again.
+    char *stringptr = GPS.lastNMEA();
+
+    if (!GPS.parse(stringptr))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+
+    // Sentence parsed!
+    Serial.println("OK");
+    if (LOG_FIXONLY && !GPS.fix) {
+      Serial.print("No Fix");
+      return;
+    }
+    
+    // Rad. lets log it!
+    Serial.println("Log");
+
+    uint8_t stringsize = strlen(stringptr);
+    if (stringsize != logfile.write((uint8_t *)stringptr, stringsize))    //write the string to the SD file
+        error(4);
+    if (strstr(stringptr, "RMC") || strstr(stringptr, "GGA") )   logfile.flush();
+    Serial.println();
+
+    float voltage = analogRead(TempSenzor) * 3.3;  //očitava vrijednosti izvoda (A0)
+    voltage /= 1024.0;
+    float Temperatura = (voltage - 0.5) * 100;
+    Serial.print("Trenutno: ");
+    Serial.println(Temperatura);
+  }
+  //Stvari za temp sensor
+  /*
+    for (int i = 0; i<10; i++)
+  {
+    float voltage = analogRead(TempSenzor) * 3.3;  //očitava vrijednosti izvoda (A0)
+    voltage /= 1024.0;
+    float Temperatura = (voltage - 0.5) * 100;
+    temp[i] = Temperatura;
+    Serial.print("Trenutno: ");
+    Serial.println(Temperatura);
+    
+    delay(150);
+  }
+  Array_sort(temp, n);
+  median = Find_median(temp, n);
+  Serial.print("Filtrirano: ");
+  Serial.println(median);
+  */
+}
