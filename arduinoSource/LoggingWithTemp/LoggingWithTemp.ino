262e59eb91839839754e5e8ac16f7a2d083a806a
Kristijan Cetina <kristijan.cetina@gmail.com>
2019-04-08 20:37:21 +0200

    Update LoggingWithTemp.ino
    Za sada dela nekako

--- arduinoSource/LoggingWithTemp/LoggingWithTemp.ino
+++ arduinoSource/LoggingWithTemp/LoggingWithTemp.ino
@@ -209,7 +209,6 @@ void Array_sort(float *array, int n)
 {
   // declare some local variables
   int i = 0, j = 0, temp = 0;
-
   for (i = 0; i<n; i++)
   {
     for (j = 0; j<n - 1; j++)
@@ -227,14 +226,12 @@ void Array_sort(float *array, int n)
 float Find_median(float array[], int n)
 {
   float median = 0;
-
   // if number of elements are even
   if (n % 2 == 0)
     median = (array[(n - 1) / 2] + array[n / 2]) / 2.0;
   // if number of elements are odd
   else
     median = array[n / 2];
-
   return median;
 }
 
